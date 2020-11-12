4c9ee605784601d6568f08960e939b98ead19cc9
John Tweeddale <j.tweeddale@outlook.com>
2020-11-12 16:29:25 +0000

    add doses create route and view

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,5 +3,22 @@ class DosesController < ApplicationController
     @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
-  
-end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)      
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+  end
