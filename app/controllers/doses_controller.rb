8a4770320737c714e204dabe9439896c5470da28
John Tweeddale <j.tweeddale@outlook.com>
2020-11-13 11:20:24 +0000

    change dose create method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,13 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:new, :create, :destroy]
+  before_action :set_dose, only: [:destroy]
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
 
     if @dose.save
